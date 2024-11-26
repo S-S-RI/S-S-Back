5b111c92d48e6605370ed9465f97db80c965eec8
Ghalem Salim <salimghalem40@gmail.com>
2024-11-26 20:51:00 +0100

    wordnet works

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 import documentRouter from './documentRoutes';
 import searchRouter from './searchRoutes';
-import { lookupWord } from '../utils/wordnetHelper';
+import { getDomainsForWord } from '../utils/wordnetHelper';
 const router = Router();
 router.use('/document', documentRouter);
 router.use('/search', searchRouter);
 router.get('/lookup/:word', async (req, res) => {
-    try {
-      const word = req.params.word; 
-      const results = await lookupWord(word);
-      res.json(results);
-    } catch (error) {
-      res.status(500);
-    }
-  });
+  try {
+    const word = req.params.word;
+    const results = await getDomainsForWord(word);
+    res.json(results);
+  } catch (error) {
+    res.status(500);
+  }
+});
 export default router;
--- src/api/v1/utils/wordnetHelper.ts
+++ src/api/v1/utils/wordnetHelper.ts
@@ -1,12 +1,50 @@
-import { WordNet } from 'natural';
+import fs from 'fs';
+import path from 'path';
+import natural from 'natural';
 
-const wordnet = new WordNet();
+const WordNet = natural.WordNet;
+const wordnet20data = path.join(__dirname, '../../../../WordNet-2.0/dict/');
+const wordnet = new WordNet(wordnet20data);
 
-export const lookupWord = async (word: string) => {
-  return new Promise((resolve, reject) => {
+const domainsFilePath = path.join(
+  __dirname,
+  '../../../../wn-domains-3.2-20070223'
+);
+
+const parseDomainsFile = (): Record<string, string[]> => {
+  const domainMap: Record<string, string[]> = {};
+  const data = fs.readFileSync(domainsFilePath, 'utf-8');
+
+  data.split('\n').forEach((line) => {
+    const [synsetId, domains] = line.split('\t');
+    if (synsetId && domains) {
+      domainMap[synsetId] = domains.split(' ');
+    }
+  });
+
+  return domainMap;
+};
+
+const domainMap = parseDomainsFile();
+export const getDomainsForWord = (word: string): Promise<string[]> => {
+  return new Promise((resolve) => {
     wordnet.lookup(word, (results) => {
-      if (results) resolve(results);
-      else reject(new Error('No results found'));
+      if (results.length === 0) {
+        resolve(['Unknown']);
+        return;
+      }
+      console.log(results);
+      const domains = results.flatMap((result) => {
+        const synsetId = `${result.synsetOffset.toString().padStart(8, '0')}-${
+          result.pos
+        }`;
+
+        return domainMap[synsetId] || [];
+      });
+
+      resolve([...new Set(domains)]);
     });
   });
 };
+
+export { domainMap };
